1a1345187b8e194ddad0d11bf14482ec951bcb64
motanelu <miau@motane.lu>
2016-01-24 22:42:28 +0100

    Initial commit

---
+++ trekin.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import os, sys, json, ConfigParser, trello
+
+def get_app_config():
+    config_path = os.path.expanduser('~/.trello-kindle-config')
+    if os.path.exists(config_path) == False:
+        raise Exception("Cannot find config file: " + config_path)
+
+    config = ConfigParser.SafeConfigParser()
+    fp = open(config_path, 'r')
+    config.readfp(fp)
+    fp.close()
+
+    return {k: v for k,v in config.items('Config')}
+
+def get_json_file(f):
+    if os.path.exists(f) == False:
+        raise Exception("Cannot find file: " + f)
+
+    return json.load(open(f, 'r'))
+
+def main():
+    if len(sys.argv) != 2:
+        raise Exception('Usage: %s <path_to_json>' % (sys.argv[0]))
+
+    json   = get_json_file(sys.argv[1])
+    config = get_app_config()
+    cards  = trello.Cards(config['api_key'], config['api_token'])
+
+    for item in json['highlights']:
+        cards.new(item['text'], config['list_id'], item['location']['url'])
+
+if __name__ == '__main__':
+    code = 0
+    try:
+        main()
+    except Exception as e:
+        print e
+        code = 1
+
+    sys.exit(code)
